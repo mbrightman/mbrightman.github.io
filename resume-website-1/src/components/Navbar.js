001d630ee6ba6c87d2a01b62d95a1a8c00679282
mbrightman <brightman.matthew@gmail.com>
2023-04-04 17:20:09 -0400

    Completed Navbar & Button components and added MainSection

--- resume-website-1/src/components/Navbar.js
+++ resume-website-1/src/components/Navbar.js
@@ -1,18 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css' 
+import { Button } from './Button'
 
 
 function Navbar() {
   const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
 
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+        setButton(false)
+    } else {
+        setButton(true)
+    }
+  }
+
+  useEffect(() => {
+    showButton()
+  }, [])
+
+  window.addEventListener('resize', showButton)
 
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className='navbar-logo'>
+                <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                     MB
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
@@ -40,7 +57,6 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
-                
             </div>
         </nav>
     </>
