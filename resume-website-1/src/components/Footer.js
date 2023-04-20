44b5bd1c0b40cdb50dd9718fe4baab83705cd902
mbrightman <brightman.matthew@gmail.com>
2023-04-19 23:20:54 -0400

    Created Footer JSX

--- resume-website-1/src/components/Footer.js
+++ resume-website-1/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Button } from './Button'
+import './Footer.css'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
@@ -16,6 +18,41 @@ function Footer() {
                 <input className='footer-input' type='email' name='email' placeholder='Your Email' />
                 <Button buttonStyle='btn--outline'>Subscribe</Button>
             </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+            <div className='footer-link-items'>
+                <h2>About Me</h2>
+                <Link to='/education'>What I learned</Link>
+                <Link to='/work'>Where I've Worked</Link>
+                <Link to='/skills'>My Skills</Link>
+                <Link to='/skills'>Fun Projects</Link>
+            </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+            <div className='footer-logo'>
+                <Link to='/' className='social-logo'>
+                    MB
+                </Link>
+            </div>
+            <small className='website-rights'>MB 2023</small>
+            <div className='social-icons'>
+                <Link className='social-icon-link linkedin' to='/' target='_blank' aria-label='LinkedIn'>
+                    <i className='fab fa-linkedin'></i>
+                </Link>
+                <Link className='social-icon-link github' to='/' target='_blank' aria-label='GitHub'>
+                    <i className='fab fa-github'></i>
+                </Link>
+                <Link className='social-icon-link instagram' to='/' target='_blank' aria-label='Instagram'>
+                    <i className='fab fa-instagram'></i>
+                </Link>
+                <Link className='social-icon-link twitter' to='/' target='_blank' aria-label='Twitter'>
+                    <i className='fab fa-twitter'></i>
+                </Link>
+            </div>
         </div>
       </section>
     </div>
