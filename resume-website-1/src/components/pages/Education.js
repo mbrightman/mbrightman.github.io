254faf78c0df558999d3227402311d89676f1487
mbrightman <brightman.matthew@gmail.com>
2023-04-20 00:05:45 -0400

    Began adding to Education page

--- resume-website-1/src/components/pages/Education.js
+++ resume-website-1/src/components/pages/Education.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import '../../App.css'
+import './Education.css'
 
 export default function Education() {
-    return <h1 className='education'>EDUCATION</h1>
+    return(
+        <div className='education'>
+            <h1>EDUCATION</h1>
+        </div>
+    ) 
+    
 }
