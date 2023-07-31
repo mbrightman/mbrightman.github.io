7097393cd3dd8b2c7b3d44d5233b7179c5c336ef
mbrightman <brightman.matthew@gmail.com>
2023-07-31 01:52:26 -0400

    Education page additions: Internships, formatting, extracurriculars, coursework, etc

--- resume-website-1/src/components/EducationMain.js
+++ resume-website-1/src/components/EducationMain.js
@@ -4,69 +4,113 @@ import './EducationMain.css'
 
 function EducationMain() {
     return(
-        <div className='education-details'>
-            <h2>Degrees Earned</h2>
-
-            <div className='education-degrees'>
-                <p>Bachelor of Science in Computer Science</p>
-                <p>Bachelor of Arts in Mathematics</p>
-                <p>Minor in Statistics</p>
-                <p>GPA: 3.4</p>
+        <>
+            <div className='education-tagline'>
+                <h1>Tarheel Born,<br/></h1>
+                <h1 style={{marginLeft:200}}>Tarheel Bred</h1>
             </div>
 
-            <p>This is a quick paragraph summarizing my time at UNC.</p>
-            <br/>
-            <h3>Coursework</h3>
-            <div className='coursework-container'>
-                <div className='column'>
-                    <p>Python</p>
-                    <p>JavaScript</p>
-                    <p>Java</p>
-                    <p>C#</p>
-                    <p>ReactJS</p>
+            <div className='education-details'>
+                <h2>University of North Carolina at Chapel Hill</h2>
+
+                <div className='education-degrees'>
+                    <p>Bachelor of Science in Computer Science</p>
+                    <p>Bachelor of Arts in Mathematics</p>
+                    <p>Minor in Statistics</p>
+                    <p>GPA: 3.4</p>
                 </div>
-                <div className='column'>
-                    <p>Cryptography</p>
-                    <p>Password Cracking</p>
-                    <p>Hash Encryption</p>
+
+                <p>This is a quick paragraph summarizing my time at UNC. See more about my time at UNC!</p>
+                <br/>
+                <h3>Relevant Coursework</h3>
+                <div className='coursework-container'>
+                    <div className='column'>
+                        <p>JavaScript</p>
+                        <p>Java</p>
+                        <p>C#</p>
+                        <p>ReactJS</p>
+                        <p>Algorithms & Analysis</p>
+                    </div>
+                    <div className='column'>
+                        <p>Cryptography</p>
+                        <p>Password Cracking</p>
+                        <p>Hash Encryption</p>
+                        <p>Security Principles</p>
+                        <p>Network Protocols</p>
+                    </div>
+                    <div className='column'>
+                        <p>Python</p>
+                        <p>Internet Services & Programming</p>
+                        <p>R</p>
+                        <p>SQL</p>
+                        <p>Data Structures</p>
+                    </div>
                 </div>
-                <div className='column'>
-                    <p>R</p>
-                    <p>MySQL</p>
-                    <p>Web Development</p>
+                <br/>
+                <h3>Internships</h3>
+                <div className='internship-table'>
+                    <table>
+                        <tr>
+                            <td>
+                                <h4>Web Development Intern, TIAA</h4>
+                                <p style={{marginLeft: 30}}>Assisted with features and integration of a web application using Angular and 
+                                    Java, including developing features, improving UI, and demoing the application 
+                                    for associates. Also responsible for developing a quantitative method for tracking 
+                                    associate training for gamification purposes.</p>
+                            </td>
+                            <td><h4>June 2020 - August 2020</h4></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h4>Vulernability Management Cyber Security Intern, TIAA</h4>
+                                <p style={{marginLeft:30}}>Worked with teammates to identify and remediate infrastructure vulnerabilities 
+                                    using Python, SQL, and web applications including Nexpose and Splunk. 
+                                    Developed a Splunk Dashboard and worked on Python automation scripts.
+                                    </p>
+                            </td>
+                            <td>
+                                <h4>June 2021 - August 2021</h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h4>Software Quality Assurance Tester, RENCI</h4>
+                                <p style={{marginLeft:30}}>Manually tested web applications created by RENCI to ensure that all functionality 
+                                within the applications worked correctly. Began development of Python automated testing for RENCI applications.
+                                Began writing documentation for software products.</p>
+                            </td>
+                            <td>
+                                <h4>August 2020 - May 2021</h4>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <br/>
+                <h3>Projects</h3>
+                <div className='projects-container'>
+                    <p>UNC Baseball Analytics</p>
+                    <p>Web Development: COVID Dashboard</p>
+                    <p>MATLAB Math Projects</p>
+                    <p>Python Password Cracking - Dictionary Attacks</p>
+                </div>
+                <br/>
+                <h3>Online Courses</h3>
+                <div className='online-courses-container'>
+                    <h4>AWS Cloud Practitioner</h4>
+                    <h4>Hack The Box</h4>
+                    <h4>CompTIA Security+</h4>
+                    <h4>AWS Certifed Solutions Architect</h4>
+                </div>
+                <br/>
+                <h3>Extracurriculars</h3>
+                <div className='extracurriculars-container'>
+                    <h4>COMP 110 Hackathon</h4>
+                    <h4>Intramural Sports</h4>
+                    <h4>Cru</h4>
                 </div>
             </div>
-            <br/>
-            <h3>Internships</h3>
-            <div className='internship-container'>
-                <div className='internship'>
-
-                </div>
-                <div className='internship'>
-
-                </div>
-                <div className='internship'>
-
-                </div>
-            </div>
-            <br/>
-            <h3>Projects</h3>
-            <div className='projects-container'>
-                <p>UNC Baseball Analytics</p>
-                <p>Web Development: COVID Dashboard</p>
-                <p>MATLAB Math Projects</p>
-                <p>Python Password Cracking - Dictionary Attacks</p>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-        </div>
+        </>
+        
 
     )
 }
--- resume-website-1/src/components/pages/Education.js
+++ resume-website-1/src/components/pages/Education.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import '../../App.css'
-import './Education.css'
 import EducationMain from '../EducationMain.js'
 
 export default function Education() {
     return(
         <>
             <div className='education'>
-            <h1>EDUCATION</h1>
+                <h1>EDUCATION</h1>
             </div>
             
+
             <EducationMain />
             
         </>
