b216527ba826d907cf5bd60d6680740347c812f7
mbrightman <brightman.matthew@gmail.com>
2025-07-22 11:03:55 -0400

    Updates to Skills page - Certs Carousel and Skills List

--- resume-website-1/src/components/SkillsMain.js
+++ resume-website-1/src/components/SkillsMain.js
@@ -11,8 +11,50 @@ import weatherLogo from 'C:/Users/mattb523/Documents/my-website/my-website/resum
 function SkillsMain() {
     return(
         <>
-            <h4>Certifications</h4>
-            <CertsCarousel />
+            <div className='certs-skills-box'>
+                <h2>Certifications and Skills</h2>
+                <div className='certs-skills-columns'>
+                    <div className='certs-column'>
+                        <CertsCarousel />
+                    </div>
+                    <div className='skills-column'>
+                        <div className='skills-details'>
+                            <div className='skills-container'>
+                                <div className='column'>
+                                    <h4>Languages</h4>
+                                    <p>Python</p>
+                                    <p>JavaScript</p>
+                                    <p>SQL</p>
+                                    <p>Java</p>
+                                    <p>Splunk</p>
+                                    <p>HTML/CSS</p>
+                                    <p>R</p>
+                                    <p>C</p>
+                                </div>
+                                <div className='column'>
+                                    <h4>Frameworks</h4>
+                                    <p>AWS</p>
+                                    <p>React</p>
+                                    <p>Node.js</p>
+                                    <p>Angular</p>
+                                    <p>Google Cloud Platform</p>
+                                </div>
+                                <div className='column'>
+                                    <h4>Developer Tools</h4>
+                                    <p>Git</p>
+                                    <p>Visual Studio</p>
+                                    <p>Eclipse</p>
+                                    <p>Anaconda</p>
+                                    <p>Debian</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+                
+
+
 
             <div className='skills-details'>
                 <div className='skills-container'>
